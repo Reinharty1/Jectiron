2f014e2fedaffb75cd5fa43af3770b4bd2b26d1b
Reinharty1 <dr.reinny@gmail.com>
2025-09-16 13:38:41 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+const CACHE = "ferosac-cache-v1";
+const ASSETS = ["/", "/index.html", "/manifest.webmanifest", "/icons/icon-192.png", "/icons/icon-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
